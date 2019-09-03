0864572645b9f46ce463341651bb4fb19b831202
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-09-03 16:59:29 -0700

    nameInput and number reducers

--- src/reducers/contacts.js
+++ src/reducers/contacts.js
@@ -1,12 +1,18 @@
+import { UPDATE_NAMEINPUT, UPDATE_NUMBERINPUT } from '../actions/contactActions';
+
 const initialState = {
   contacts: [],
   searchInput: '',
   nameInput: '',
-  phoneInput: ''
+  numberInput: ''
 };
 
 export default function reducer(state = initialState, action){
   switch(action.type){
+    case UPDATE_NAMEINPUT: 
+      return{ ...state, nameInput: action.payload };
+    case UPDATE_NUMBERINPUT: 
+      return{ ...state, numberInput: action.payload };
     default: return state;
   }
 }
