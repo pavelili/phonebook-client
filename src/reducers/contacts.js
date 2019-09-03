66a6b6d08197f895ce4a28773daa26b3c4cb009d
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-09-03 16:40:00 -0700

    reducers files setup

---
+++ src/reducers/contacts.js
@@ -0,0 +1,12 @@
+const initialState = {
+  contacts: [],
+  searchInput: '',
+  nameInput: '',
+  phoneInput: ''
+};
+
+export default function reducer(state = initialState, action){
+  switch(action.type){
+    default: return state;
+  }
+}
