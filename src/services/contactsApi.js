704b9d087ae05d463d52a0c740535b6c9d5868c3
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-09-03 18:02:37 -0700

    addContactApi fetch call

--- src/services/contactsApi.js
+++ src/services/contactsApi.js
@@ -1,4 +1,4 @@
-const url = '';
+const url = 'https://pure-woodland-72976.herokuapp.com/api/v1/contacts';
 
 export const getContacts = () => {
   return fetch(url)
@@ -6,4 +6,15 @@ export const getContacts = () => {
       if(!res.ok) throw 'Uhh, yeahhhh... You\'re note getting any contacts.';
       return res.json();
     });
+};
+
+export const addContactApi = (name, phoneNumber) => {
+  return fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ name, phoneNumber })
+  })
+    .then(res => res.json());
 };
