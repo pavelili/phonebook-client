51c31fdf48875dc1e3a6412aed7ff5e270b7b09b
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-09-03 18:02:11 -0700

    selectors to get all the inputs

---
+++ src/selectors/contactSelectors.js
@@ -0,0 +1,3 @@
+export const getNameInput = state => state.contacts.nameInput;
+export const getNumberInput = state => state.contacts.numberInput;
+export const getContactsArr = state => state.contacts.contacts;
